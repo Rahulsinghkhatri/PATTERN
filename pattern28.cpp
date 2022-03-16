f7603ecb6f25d66ee4256d48cf401dcab4f23d5d
RAHUL SINGH KHATRI <rahulsinghkhatri2002@gmail.com>
2022-03-16 11:53:02 +0530

    Add files via upload

---
+++ pattern28.cpp
@@ -0,0 +1,43 @@
+/*
+      1
+    1 2 1
+  1 2 3 2 1
+1 2 3 4 3 2 1
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n-i)
+        {
+            cout<<"  ";
+            j++;                
+        }
+
+        int k=1;
+        while(k<=i)
+        {
+            cout<<k<<" ";
+            k++;
+        }
+
+        int l=i-1;
+        while(l)
+        {
+            cout<<l<<" ";
+            l--;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
